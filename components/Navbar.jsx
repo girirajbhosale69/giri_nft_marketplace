9bc0adcfb35ef7bc79a337673eb040d053e8e0d4
GIRIRAJ SATISH BHOSALE <74863022+girirajbhosale69@users.noreply.github.com>
2022-09-10 19:23:52 +0530

    create navbar (in process)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+import images from '../assets';
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">CryptoKet</p>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
