6b4331dffbbac7323dbf8a5efd03b0c0fde170c8
GIRIRAJ SATISH BHOSALE <74863022+girirajbhosale69@users.noreply.github.com>
2022-09-20 02:22:34 +0530

    rpc fix chain id

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,7 @@ const privateKey = fs.readFileSync('.secret').toString().trim();
 module.exports = {
   networks: {
     hardhat: {
-      url: process.env.NEXT_PUBLIC_API_ENDPOINT,
-      chainId: 1337,
+      chainId: 31337,
     },
   },
   solidity: '0.8.4',
